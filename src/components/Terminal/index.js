6d75e80fe472617a868ef7cc20ae1cc1b7c0fbfe
Pablo Lazo <lazopm@gmail.com>
2017-07-15 19:07:03 -0400

    Install aphrodite

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'aphrodite';
+
+import styles from './styles';
 
 const Terminal = () =>
-    <div>
-        terminal!
+    <div className={css(styles.Container)}>
     </div>
 
 
---
+++ src/components/Terminal/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'aphrodite';
+import { bgDark } from 'constants/colors';
+
+export default StyleSheet.create({
+    Container: {
+        background: bgDark, 
+        height: 480,
+        width: 640,
+    }
+});
