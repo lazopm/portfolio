382cd6e037ca9b425baa68bef01cd79db437dd9b
Pablo Lazo <lazopm@gmail.com>
2017-07-16 02:07:51 -0400

    Add sprite option for Icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { textDark } from 'constants/colors';
 
-const Icon = ({ name, color }) =>
+const Icon = ({ name, color, sprite }) =>
     <svg style={{
         width: '48px',
         height: '48px',
     }}>
         <use
             xmlnsXlink="http://www.w3.org/1999/xlink"
-            xlinkHref={`/icons/regular.svg#${ name }`}
+            xlinkHref={`/icons/${sprite}.svg#${name}`}
         />
     </svg>;
 
 Icon.defaultProps = {
     color: textDark,
+    sprite: 'regular',
 };
 
 Icon.propTypes = {
     name: PropTypes.string.isRequired,
-    color: PropTypes.string,
+    color: PropTypes.string.isRequired,
+    sprite: PropTypes.string.isRequired,
 };
 
 export default Icon;
