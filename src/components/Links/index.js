bfb6349b39bccfef674b412e565cd53905f705e1
Pablo Lazo <lazopm@gmail.com>
2017-07-18 01:12:40 -0400

    Update link text

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -37,7 +37,7 @@ const Links = () =>
             icon="file-alt"
         />
         <Link
-            text="hi@lazopm.com"
+            text="email"
             icon="envelope"
             href="mailto:hi@lazopm.com"
         />
