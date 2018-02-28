9e498e50e40fd4763228b63c3030dfa825c9c20c
Pablo Lazo <lazopm@gmail.com>
2018-02-28 05:22:02 -0500

    Get rid of unnecessary stream stylesheet consolidation

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { hydrate } from 'react-dom'
-import { consolidateStreamedStyles } from 'styled-components'
 
 const AppModulePromise = import(/* webpackChunkName: "app" */ './App');
-consolidateStreamedStyles();
 
 AppModulePromise.then(module => {
     const App = module.default;
