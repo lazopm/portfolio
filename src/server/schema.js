b30732ed95101ce5ac612d0dbdb67aea7b511d82
Pablo Lazo <lazopm@gmail.com>
2018-02-28 17:25:06 -0500

    Add initial graphql project api

--- src/server/schema.js
+++ src/server/schema.js
@@ -1,41 +1,65 @@
-const { makeExecutableSchema } = require('graphql-tools');
+import { makeExecutableSchema } from 'graphql-tools';
+import { ObjectId } from 'mongodb';
 
 const typeDefs = `
-    type Project {
-        name: String
-        date: String
-    }
-    type Query {
-        project(id: ID!): Project
-        projects: [Project]
-    }
+	type Project {
+		id: ID!
+		name: String!
+		created: String!
+	}
+	type Query {
+		project(id: ID!): Project
+		projects: [Project!]
+	}
+	type Mutation {
+		addProject(name: String!): Project
+	}
 `;
 
+const toProject = ({ _id, created, ...fields }) => ({
+	...fields,
+	id: _id.toString(),
+	created: String(created),
+});
+
 const resolvers = {
-    Query: {
-        project: (...args) => {
-            return { 
-                id: '123',
-                name: 'split'
-            };
-        },
-        projects: () => {
-            return [{ 
-                id: '123',
-                name: 'split'
-            }];
-        }
-    },
-    Project: {
-        date: (...args) => {
-            return 'hehexd';
-        },
-  },
+	Query: {
+		project: async (_, { id }) => {
+			const collection = db.collection('projects');
+			let _id
+			try {
+				_id = ObjectId(id);
+			} catch(err) {
+				throw new Error('Invalid id');
+			}
+			const project = await collection.findOne({ _id });
+			if (project) {
+				return toProject(project);
+			}
+		},
+		projects: async () => {
+			const collection = db.collection('projects');
+			const projects = await collection.find({}).toArray();
+			return projects.map(toProject);
+		}
+	},
+	Mutation: {
+		addProject: async (_, args) => {
+			const collection = db.collection('projects');
+			const result = await collection.insertOne({
+				...args,
+				created: Date.now(),
+			});
+			if (result.insertedCount === 1) {
+				return toProject(result.ops[0]);
+			}
+		}
+	},
 };
 
 const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers,
 });
 
 export default schema;
