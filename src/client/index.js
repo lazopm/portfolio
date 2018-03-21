0a9f05b779cd390e5f0551d45d06dbe027d3d003
Pablo Lazo <lazopm@gmail.com>
2018-03-21 14:56:12 -0400

    Bundle optimizations

--- src/client/index.js
+++ src/client/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import { hydrate } from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const asyncImports = [
+    import(/* webpackChunkName: "app" */ './App'),
+    import(/* webpackChunkName: "app" */'react-dom'),
+    import(/* webpackChunkName: "app" */'react-apollo'),
+];
 
 const client = new ApolloClient({
     link: new HttpLink(),
     cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
 
-const AppModulePromise = import(/* webpackChunkName: "app" */ './App');
-
-AppModulePromise.then(module => {
-    const App = module.default;
-    const app = (
+Promise.all(asyncImports).then(([
+    {default: App},
+    { hydrate },
+    { ApolloProvider },
+]) =>
+    hydrate(
         <ApolloProvider client={client}>
             <App />
-        </ApolloProvider>
-    );
-    hydrate(app, document.getElementById('root'));
-});
+        </ApolloProvider>,
+        document.getElementById('root')
+    )
+);
