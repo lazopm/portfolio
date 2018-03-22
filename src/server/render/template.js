7f42dc7594f1cb0a9bdad1fb37e25d7c66f3ee04
Pablo Lazo <lazopm@gmail.com>
2018-03-22 11:55:57 -0400

    SEO tweaks

--- src/server/render/template.js
+++ src/server/render/template.js
@@ -5,10 +5,11 @@ export default (html, css, apolloState) => `
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="theme-color" content="#000000">
-        <meta name="description" content="Pablo Lazo, Software Developer Portfolio"/>
+        <meta name="description" content="Pablo Lazo, Software Developer Portfolio. Pablo is a Chilean developer based in Washington, DC."/>
+        <meta name="keywords" content="pablo lazo, lazopm, software developer, portfolio, web developer, front end developer"/>
         <link rel="manifest" href="/assets/manifest.json">
         <link rel="shortcut icon" href="/assets/favicon.ico">
-        <title>Pablo Lazo - Software Developer</title>
+        <title>lazopm - Pablo Lazo's Software Developer Porftolio</title>
         ${css}
       </head>
       <body>
