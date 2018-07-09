9b65cdfedf1c0fa54fd77a29d2f2a2bfd0540558
Pablo Lazo <lazopm@gmail.com>
2018-07-09 04:45:40 -0400

    Use new Query component

--- src/client/components/Projects/index.js
+++ src/client/components/Projects/index.js
@@ -1,78 +1,83 @@
 import React from 'react';
 import styled from 'styled-components';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Project from './Project';
 
-const Projects = ({ projects }) => (
-    <React.Fragment>
-        {projects.map(project => <Project key={project.id} {...project} />)}
-    </React.Fragment>
-);
-
-Projects.defaultProps = {
-    projects: [],
-};
-
-export default graphql(
-    gql`
-        query {
-            user(login: "lazopm") {
-                pinnedRepositories(first: 6) {
-                    nodes {
-                        id
-                    }
+const query = gql`
+    query {
+        user(login: "lazopm") {
+            pinnedRepositories(first: 6) {
+                nodes {
+                    id
                 }
-                repositories(
-                    privacy: PUBLIC
-                    isFork: false
-                    first: 30
-                    affiliations: [OWNER]
-                    orderBy: { field: CREATED_AT, direction: DESC }
-                ) {
-                    nodes {
-                        id
-                        name
-                        description
-                        demoUrl: homepageUrl
-                        sourceUrl: url
-                        readmeMarkdown: object(expression: "master:README.md") {
-                            ... on Blob {
-                                text
-                            }
+            }
+            repositories(
+                privacy: PUBLIC
+                isFork: false
+                first: 30
+                affiliations: [OWNER]
+                orderBy: { field: CREATED_AT, direction: DESC }
+            ) {
+                nodes {
+                    id
+                    name
+                    description
+                    demoUrl: homepageUrl
+                    sourceUrl: url
+                    readmeMarkdown: object(expression: "master:README.md") {
+                        ... on Blob {
+                            text
                         }
-                        portfolioMarkdown: object(
-                            expression: "master:PORTFOLIO.md"
-                        ) {
-                            ... on Blob {
-                                text
-                            }
+                    }
+                    portfolioMarkdown: object(
+                        expression: "master:PORTFOLIO.md"
+                    ) {
+                        ... on Blob {
+                            text
                         }
-                        topics: repositoryTopics(first: 10) {
-                            nodes {
-                                topic {
-                                    name
-                                }
+                    }
+                    topics: repositoryTopics(first: 10) {
+                        nodes {
+                            topic {
+                                name
                             }
                         }
                     }
                 }
             }
         }
-    `,
-    {
-        props: ({ data }) => {
+    }
+`;
+
+const Projects = () => (
+    <Query query={query}>
+        {({ data }) => {
             const allProjects = data.user.repositories.nodes.map(project => ({
                 ...project,
                 topics: project.topics.nodes.map(node => node.topic.name),
             }));
             const pinnedIds = data.user.pinnedRepositories.nodes
-                .map(node => node.id)
-                .reverse();
+                .map(node => node.id);
             const projects = allProjects.sort(
-                (a, b) => pinnedIds.indexOf(a.id) < pinnedIds.indexOf(b.id),
+                (a, b) => {
+                    const ai = pinnedIds.indexOf(a.id);
+                    const bi = pinnedIds.indexOf(b.id);
+                    if (ai === -1 || bi === -1) return 0;
+                    return ai - bi;
+                }
             );
-            return { projects };
-        },
-    },
-)(Projects);
+            return (
+                <React.Fragment>
+                    {projects.map(project => <Project key={project.id} {...project} />)}
+                </React.Fragment>
+            );
+        }}
+    </Query>
+);
+
+Projects.defaultProps = {
+    projects: [],
+};
+
+export default Projects;
