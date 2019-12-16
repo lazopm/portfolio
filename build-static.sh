f3c137fbf1e5b249b970620aaa4359f733c893c5
Pablo Lazo <lazopm@gmail.com>
2019-12-15 21:34:33 -0800

    Update build script

--- build-static.sh
+++ build-static.sh
@@ -1,10 +1,7 @@
 # build source
-rm -rf build
-npm run build
+[ ! -d "build" ] && npm run build
 
-# github pages  
-rm -rf docs
-mkdir docs
+[ ! -d "docs" ] && mkdir docs
 node build/render.bundle.js > docs/index.html
 cp -a src/assets/. docs/
 cp -a build/bundles/. docs/
